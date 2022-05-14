bcb833a2a03f8ca7ce2297490982f589e5b5191c
Shahinur01 <shahinurislam903@gmail.com>
2022-05-14 13:57:17 +0600

    regular expression

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// #1 Create an email pattern 
+/* let text ='DonaldRFalbo@rhyta.com';
+
+let pattern =/^[a-zA-Z0-9_.]*@[a-zA-Z0-9]*\.[a-z]{3,}$/;
+
+console.log(pattern.test(text)); */
+
+
+// #2 Create a  username pattern
+/* let username ="shahinur";
+let pattern =/^[a-zA-Z0-9]*$/
+
+console.log(pattern.test(username)); */
+
+
+// #3 Create a Bangladeshi phone number pattern
+// let number ="01762033852";
+// let pattern =/^(\+8801|01|00801)[0-9]{9}$/;
+// console.log(pattern.test(number));
+
+// #4 Create a password pattern 
+/* 
+let password ="y2-ku6G#VTk8k$q6";
+let pattern =/^[a-zA-Z0-9!@#$%^&*()_+-]*$/;
+
+console.log(pattern.test(password)); */
+
+// #5 Create a zipcode pattern 
+let zipcode ="12345-6789";
+let pattern =/^[0-9-]*$/
+console.log(pattern.test(zipcode));
+
