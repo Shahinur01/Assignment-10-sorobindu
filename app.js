eb635b47f19678fd308e6719a142cecfca94a1fe
Shahinur01 <shahinurislam903@gmail.com>
2022-06-06 22:18:29 +0600

    done

--- app.js
+++ app.js
@@ -20,14 +20,34 @@ console.log(pattern.test(username)); */
 // console.log(pattern.test(number));
 
 // #4 Create a password pattern 
-/* 
-let password ="y2-ku6G#VTk8k$q6";
-let pattern =/^[a-zA-Z0-9!@#$%^&*()_+-]*$/;
 
-console.log(pattern.test(password)); */
+// let password ="y2-ku6G#VTk8k$q6";
+// let pattern =/^[a-zA-Z0-9!@#$%^&*()_+-]*$/;
+
+// console.log(pattern.test(password));
 
 // #5 Create a zipcode pattern 
-let zipcode ="12345-6789";
+/* let zipcode ="12345-6789";
 let pattern =/^[0-9-]*$/
-console.log(pattern.test(zipcode));
+console.log(pattern.test(zipcode)); */
 
+// const product = 5; 
+// const price = "7"; 
+// const subTotal = product * price; 
+// const tax = subTotal/10; 
+// const total = subTotal + tax; 
+// console.log(total);
+// const category="pen"; 
+// const performer = category+"-teller";
+// console.log(performer);
+// const str1 = 'Bangladesh is a beautiful country.'; 
+// console.log(str1.endsWith("country"));
+
+
+// const array = ["121", "12", "1", "112", "111"];
+
+// console.log(array.sort());
+const task = "Practice Makes a person perfect";
+const index = task.toLowerCase().indexOf("M");
+console.log(index);
+// done
